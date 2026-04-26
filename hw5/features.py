271f8d8fe0a6937b24b05a87cc6b08ef81a818eb
Ваше Имя <you@example.com>
2026-04-26 22:58:30 +0300

    fix

---
+++ hw5/features.py
@@ -0,0 +1,30 @@
+from feast import Entity, FeatureView, FileSource, Field
+from feast.types import Float32, Int64
+import pandas as pd
+from datetime import timedelta, datetime
+
+# Читаем iris.csv из родительской папки и сохраняем как parquet
+df = pd.read_csv("../data/iris.csv")
+df["event_timestamp"] = datetime.now()
+df["id"] = range(len(df))
+df.to_parquet("data/iris.parquet", index=False)
+
+iris_entity = Entity(name="iris_id", join_keys=["id"], value_type=Int64)
+
+iris_source = FileSource(
+    path="data/iris.parquet",
+    timestamp_field="event_timestamp",
+)
+
+iris_features = FeatureView(
+    name="iris_features",
+    entities=[iris_entity],
+    ttl=timedelta(days=1),
+    schema=[
+        Field(name="sepal_length", dtype=Float32),
+        Field(name="sepal_width", dtype=Float32),
+        Field(name="petal_length", dtype=Float32),
+        Field(name="petal_width", dtype=Float32),
+    ],
+    source=iris_source,
+)
